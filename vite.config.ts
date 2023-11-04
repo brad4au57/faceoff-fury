dab00acc1d250ee876647bb8cea72e01e81b9759
Brad Bales <brad4au57@gmail.com>
2023-11-03 20:21:29 -0500

    Restarted init of repo with Vite, React, and Chakra UI. Clean Start

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import react from '@vitejs/plugin-react';
-import million from 'million/compiler';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    million.vite({ auto: true }),
-    react(),
-    checker({ typescript: true, eslint: { lintCommand: 'eslint src' } }),
-    tsconfigPaths(),
-  ],
-  server: {
-    open: true,
-  },
-});
+  plugins: [react()],
+})
